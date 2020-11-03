604279213d5367d9a536ae95dc67119d13786ad8
19h47 <jeremylevron@19h47.fr>
2020-11-03 08:33:34 +0100

    :sparkles: SplitText partial

---
+++ woocommerce/loop/loop-end.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Product Loop End
+ *
+ * @package Rider404
+ */
+
+Timber::render( 'woocommerce/loop/loop-end.html.twig' );
---
+++ woocommerce/loop/loop-start.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * @package Rider404
+ */
+
+Timber::render( 'woocommerce/loop/loop-start.html.twig' );
