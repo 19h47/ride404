bb8ae0657f2ccc30db1b2667ea4a8cc4eb140735
19h47 <jeremylevron@19h47.fr>
2020-11-18 17:13:30 +0100

    :sparkles: Bind Lottie on Scroll on AboutMapBlock (thanks to Locomotive-scroll v4)

--- src/scripts/blocks/AboutMapBlock.js
+++ src/scripts/blocks/AboutMapBlock.js
@@ -1,7 +1,7 @@
 import { EventTypes } from 'starting-blocks';
 
 import AbstractLottieBlock from 'abstracts/AbstractLottieBlock';
-// import Scroll from 'common/Scroll';
+import Scroll from 'common/Scroll';
 
 /**
  *
@@ -13,15 +13,14 @@ export default class AboutMapBlock extends AbstractLottieBlock {
 		super.initEvents();
 
 		window.addEventListener(EventTypes.AFTER_SPLASHSCREEN_HIDE, () => {
-			// console.log(this.duration);
-			// Scroll.on('scroll', ({ scroll, limit }) => {
-			// const { height, top } = this.rootElement.getBoundingClientRect();
-			// console.log(height, top);
-			// const distance = scroll.y + limit - top;
-			// const percentage = Math.round(distance / ((limit + height) / 100));
-			// console.log(Math.min(100, Math.max(0, percentage)));
-			// this.animation.goToAndStop((percentage * this.duration) / 100);
-			// });
+			Scroll.on('scroll', ({ currentElements }) => {
+				// eslint-disable-next-line valid-typeof
+				if (currentElements['about-map']) {
+					const { progress } = currentElements['about-map'];
+
+					this.animation.goToAndStop(progress * this.duration, true);
+				}
+			});
 		});
 	}
 }
